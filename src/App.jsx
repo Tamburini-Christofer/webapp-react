c7b4158f1f5be3843f1e5f1519d56bdf66132d4e
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-11-03 14:13:31 +0100

    MILLESTONE 2: Impostate le rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import './App.css'
+//? Importo librerie React (poer gestire le rotte)
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+//? Importo app.css
+import "./App.css";
+
+//? Importo il layout
+import Layout from "./assets/layouts/layout";
+
+//? Importo pagine
+import Home from "./assets/pages/Home";
+import DettaglioFilm from "./assets/pages/DettaglioFilm";
 
 function App() {
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='/movie/:id' element={<DettaglioFilm />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
